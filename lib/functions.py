eacb6ad620213441dba4ccad2fdf692c39aecdf9
Domenique <domogenesis77@gmail.com>
2023-08-10 17:21:26 -0500

    Preferences Persist on Login/Logoff

--- lib/functions.py
+++ lib/functions.py
@@ -37,13 +37,16 @@ def menu():
     
 def login():
     global current_user
+    global current_prefs
     users = session.query(User)
     selection = input(chalk.green.bold("Please type your username to login or type \"new\" to create new user.\n"))
     if selection in [user.username for user in users]:
         print(chalk.green.bold("login success!"))
         global is_logged_in
         is_logged_in = True
-        current_user = session.query(User).filter(User.username == selection).first()
+        current_user = session.query(User).filter(User.username == selection).first() 
+        current_prefs = session.query(WeatherPreference).filter(WeatherPreference.user_id == User.id).first()
+        set_preference()
         menu()
     elif selection.lower() == "new":
         print(chalk.green.bold("Create new user"))
@@ -92,36 +95,42 @@ def add_user():
     menu()
 
 def add_preference():
-    global units
-    global visibs
-    global humids
-    global precips
-    global feels
+    global new_units
+    global new_precips
+    global new_humids
+    global new_feels_likes
+    global new_visibs
     new_units = input(chalk.green("Pick Metric or Imperial(american): 1 or 2\n"))
     new_precips = input(chalk.green("Would you like to add precipitation? 1 Yes 2 No\n"))
     new_humids = input(chalk.green("Would you like to add humidity? 1 Yes 2 No\n"))
     new_feels_likes = input(chalk.green("Would you like to add feels like temperature? 1 Yes 2 No\n"))
     new_visibs = input(chalk.green("Would you like to add visibility range? 1 Yes 2 No\n"))
-    preferences = WeatherPreference(preferred_units=new_units,precip_unit = new_precips,humid_unit = new_humids,feels_like_unit = new_feels_likes,visibility_unit = new_visibs)
+    preferences = WeatherPreference(preferred_units=new_units,precip_unit = new_precips,humid_unit = new_humids,feels_like_unit = new_feels_likes,visibility_unit = new_visibs, user_id = current_user.id)
     uni = session.query(WeatherPreference).filter(WeatherPreference.user_id == current_user.id).first()
     if uni:
         setattr(uni, "preferred_units", new_units)
-    unitz = WeatherPreference(preferred_units = new_units)
-    units = unitz.preferred_units
-    precipz = WeatherPreference(precip_unit = new_precips)
-    precips = precipz.precip_unit
-    humidz = WeatherPreference(humid_unit = new_humids)
-    humids = humidz.humid_unit
-    feelz = WeatherPreference(feels_like_unit = new_feels_likes)
-    feels = feelz.feels_like_unit
-    visibz = WeatherPreference(visibility_unit = new_visibs)
-    visibs = visibz.visibility_unit
+        setattr(uni, "precip_unit", new_precips)
+        setattr(uni, "humid_unit", new_humids)
+        setattr(uni, "feels_like_unit", new_feels_likes)
+        setattr(uni, "visibility_unit", new_visibs)
+
+   
+    
     session.add(preferences)
     session.commit()
     menu()
 
-def use_preference(session):
-    pass
+def set_preference():
+    global units
+    global visibs
+    global humids
+    global precips
+    global feels
+    units = current_prefs.preferred_units
+    precips = current_prefs.precip_unit
+    humids = current_prefs.humid_unit
+    feels = current_prefs.feels_like_unit
+    visibs = current_prefs.visibility_unit
 def typewriter(message):
     for char in message:
         sys.stdout.write(char)
